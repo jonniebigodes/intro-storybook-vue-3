4df25d3009e1b04c7987ad37d5d21bb9101612a1
jonniebigodes <joaocontadesenvolvimento@gmail.com>
2021-04-22 18:03:09 +0100

    Data section updates

--- src/components/PureTaskList.stories.js
+++ src/components/PureTaskList.stories.js
@@ -1,9 +1,9 @@
-import TaskList from "./TaskList.vue";
+import PureTaskList from "./PureTaskList.vue";
 import * as TaskStories from "./Task.stories";
 
 export default {
-  component: TaskList,
-  title: "TaskList",
+  component: PureTaskList,
+  title: "PureTaskList",
   decorators: [
     () => ({ template: '<div style="margin: 3em;"><story/></div>' }),
   ],
@@ -14,11 +14,11 @@ export default {
 };
 
 const Template = (args) => ({
-  components: { TaskList },
+  components: { PureTaskList },
   setup() {
     return { args, ...TaskStories.actionsData };
   },
-  template: '<TaskList v-bind="args" />',
+  template: '<PureTaskList v-bind="args" />',
 });
 
 export const Default = Template.bind({});
